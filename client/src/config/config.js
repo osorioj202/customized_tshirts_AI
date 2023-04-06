e8cff7d9f819e85c3b034e706c74610f0366b308
Juan Osorio <osorioj202@gmail.com>
2023-04-06 07:06:44 -0500

    frontend deployment commit2

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -3,7 +3,8 @@ const config = {
     backendUrl: "http://localhost:8080/api/v1/dalle",
   },
   production: {
-    backendUrl: "https://devswag.onrender.com/api/v1/dalle",
+    // backendUrl: "https://devswag.onrender.com/api/v1/dalle",
+    backendUrl: "https://customized-tshirts-ai-75q1.onrender.com/api/v1/dalle",
   },
 };
 
